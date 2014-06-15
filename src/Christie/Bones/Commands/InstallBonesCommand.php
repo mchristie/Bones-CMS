3ec4af51487b74ae58ddc5cee2fde067aa9564bd
Malcolm Christie <Malk@Malcolms-MacBook-Pro-2.local>
2014-06-15 14:44:34 +0100

    Readme

--- src/Christie/Bones/Commands/InstallBonesCommand.php
+++ src/Christie/Bones/Commands/InstallBonesCommand.php
@@ -6,6 +6,12 @@ use Illuminate\Console\Command;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputArgument;
 
+use \Config;
+
+use \Christie\Bones\Models\Site;
+use \Christie\Bones\Models\User;
+use \Christie\Bones\Libraries\Bones;
+
 class InstallBonesCommand extends Command {
 
 	/**
@@ -20,7 +26,7 @@ class InstallBonesCommand extends Command {
 	 *
 	 * @var string
 	 */
-	protected $description = 'Command description.';
+	protected $description = 'Install and configure Bones CMS.';
 
 	/**
 	 * Create a new command instance.
@@ -39,7 +45,81 @@ class InstallBonesCommand extends Command {
 	 */
 	public function fire()
 	{
-		//
+		/*
+		 *  Run all bones migrations
+		 */
+		$this->info('Running package migrations...');
+		Command::call('migrate', array('--package' => 'christie/bones'));
+		$this->info("Migrations complete.\n");
+
+		/*
+		 *  Create site if one doesn't exist
+		 */
+		if (Site::count() == 0) {
+			$this->info('We need to configure the first website, we need a URL, title, and slug (which is used for site-specific folder directories).');
+			$site 			= new Site;
+			$site->url 		= $this->ask('Enter site URL http://');
+			$site->title 	= $this->ask('Enter site title: ');
+			$site->slug 	= $this->ask('Enter site slug: ');
+			$site->save();
+
+			$this->info("Site created!\n");
+		} else {
+			$this->info('Site already configured.');
+		}
+
+		/*
+		 *  Create a user if one doesn't exist
+		 */
+		if (User::count() == 0) {
+			$this->info('We need to create the first super-user, this user will have complete CMS access.');
+			$user 				= new User;
+			$user->username		= $this->ask('Username: ');
+			$user->display_name = $this->ask('Display name: ');
+			$user->email 		= $this->ask('Email address: ');
+			$user->password 	= \Hash::make( $this->secret('Password: ') );
+			$user->site_id 		= Site::first()->id;
+			$user->level		= Bones::LEVEL_SUPER;
+			$user->save();
+
+			$this->info("User created!\n");
+		} else {
+			$this->info('User already exists.');
+		}
+
+		/*
+		 *  Copy package assets
+		 */
+		if ($this->confirm('Bones admin area requires various JS and CSS assets be copied to the public directory, would you like to do this now?')) {
+			Command::call('asset:publish');
+		}
+		$this->info(PHP_EOL);
+
+		/*
+		 *  Copy view files
+		 */
+		if ($this->confirm('Bones allows you to override any view file with your own from within the views directory, would you like to copy all the view files for you to modify?')) {
+			Command::call('view:publish', array('package' => 'christie/bones'));
+		}
+		$this->info(PHP_EOL);
+
+		/*
+		 *  Ensure the change the auth model
+		 */
+		if (Config::get('auth.model') != 'Christie\Bones\Models\User') {
+			do {
+				$this->info('You must change the auth.model config to "Christie\Bones\Models\User" to continue.');
+				$this->confirm('Have you changed it?');
+			} while(Config::get('auth.model') != 'Christie\Bones\Models\User');
+		}
+
+		/*
+		 *  Done!
+		 */
+		$this->info('Done!');
+		$this->info('Head to http://'.Site::first()->url.'/admin to get started.');
+
+
 	}
 
 	/**
@@ -50,7 +130,7 @@ class InstallBonesCommand extends Command {
 	protected function getArguments()
 	{
 		return array(
-			array('example', InputArgument::REQUIRED, 'An example argument.'),
+
 		);
 	}
 
@@ -62,7 +142,7 @@ class InstallBonesCommand extends Command {
 	protected function getOptions()
 	{
 		return array(
-			array('example', null, InputOption::VALUE_OPTIONAL, 'An example option.', null),
+
 		);
 	}
 
